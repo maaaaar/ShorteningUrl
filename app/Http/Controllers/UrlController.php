a0f784f02c4f73e6cb392fe540c037a867ef8f1b
Mar Badia <mar.badia@tiendeo.com>
2023-07-11 21:43:16 +0200

    new-FunctionShortUrl

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Str;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class UrlController extends Controller
+{
+    public function acortarUrl(Request $request)
+    {
+        // Validar y procesar los datos del formulario
+        $request->validate([
+            'url' => 'required|url',
+            'alias' => 'nullable|url',
+        ]);
+
+        $urlOriginal = $request->input('url');
+        $alias = $request->input('alias');
+
+        // Generar un código único para la URL acortada
+        $codigo = substr(md5($urlOriginal), 0, 8);
+
+        // preg_match para coger la url_base
+        if (preg_match('/^(.*?.(\w{2,3}))\//is',  $urlOriginal, $dato)) {
+            $url_base = $dato[1];
+
+            if (!empty($alias)) {
+                $urlAcortada = $alias . '/' . $codigo;
+            } else {
+                $urlAcortada = $url_base . '/' . $codigo;
+            }
+        }
+        $this->guardarUrlEnJson($codigo, $urlOriginal, $urlAcortada);
+        $urls = $this->obtenerUrlsDesdeJson();
+
+        // Mando la url a la pagina index para ver el resultado
+        return view('index', ['urlAcortada' => $urlAcortada, 'urls' => $urls]);
+    }
+
+    public function mostrarUrl($urlAcortada)
+    {
+        $jsonFile = storage_path('urls.json');
+        $urls = [];
+
+        if (file_exists($jsonFile)) {
+            $jsonContent = file_get_contents($jsonFile);
+            if ($jsonContent === false) {
+                // Error al leer el archivo JSON
+                die("Error al leer el archivo JSON");
+            }
+            $urls = json_decode($jsonContent, true);
+        }
+
+        return view('mostrar-url', compact('urlAcortada', 'urls'));
+    }
+    private function guardarUrlEnJson($codigo, $urlOriginal, $urlAcortada)
+    {
+        $jsonFile = storage_path('urls.json');
+        $urls = [];
+
+        if (file_exists($jsonFile)) {
+            $jsonContent = file_get_contents($jsonFile);
+            if ($jsonContent === false) {
+                // Error al leer el archivo JSON
+                die("Error al leer el archivo JSON");
+            }
+            $urls = json_decode($jsonContent, true);
+        }
+
+        $urls[] = [
+            'codigo' => $codigo,
+            'urlOriginal' => $urlOriginal,
+            'urlAcortada' => $urlAcortada,
+        ];
+
+        file_put_contents($jsonFile, json_encode($urls));
+    }
+
+    private function obtenerUrlsDesdeJson()
+    {
+        $jsonFile = storage_path('urls.json');
+        $urls = [];
+
+        if (file_exists($jsonFile)) {
+            $jsonContent = file_get_contents($jsonFile);
+            if ($jsonContent === false) {
+                // Error al leer el archivo JSON
+                die("Error al leer el archivo JSON");
+            }
+            $urls = json_decode($jsonContent, true);
+        }
+
+        return $urls;
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('titulo')</title>
+
+    <!-- para que vaya bootsrap -->
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- CSS MIAS -->
+    <!-- <link rel="stylesheet" href="{{ asset ('css/miCss.css')}}"> -->
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-lg bg-primary navbar-fixed-top">
+        <a class="navbar-brand" href="">
+            <img src="" style="height: 50px">
+        </a>
+        <ul class="navbar-nav mr-auto">
+            <!-- <li class="nav-item ">
+                <a class="nav-link text-white" href="/index"> User </a>
+            </li> -->
+            <!-- <li class="nav-item ">
+                <a class="nav-link text-white" href="" role="button"> Analiticas </a>
+            </li> -->
+        </ul>
+    </nav>
+
+
+    <!-- form enviar url -->
+    <h2 class="text-center mx-sm-3 mb-2 m-4">Sólo ingresa tu URL y clickea "Acortar"</h2>
+    <form action="{{ route('acortar-url') }}" method="POST">
+        <div class="form-group mx-sm-3 mb-2 m-4">
+            <label for="exampleInputEmail1">URL: </label>
+            <input type="url" class="form-control sm-2" name="url" aria-describedby="url" placeholder="https://www.google.es" required>
+            <small id="urlHelp" class="form-text text-muted"></small>
+        </div>
+        <div class="form-group mx-sm-3 mb-2 m-4">
+            <label for="exampleInputEmail1">¿Quieres añadir un alias? </label>
+            <input type="url" class="form-control sm-2" name="alias" aria-describedby="alias" placeholder="https://www.alias.es">
+            <small id="urlHelp" class="form-text text-muted"></small>
+        </div>
+        <div class="col text-center">
+            @csrf
+            <button type="submit" class="btn btn-primary mb-2">Acortar</button>
+        </div>
+    </form>
+
+    <!-- <h2>Tabla con las urls</h2> -->
+    <table class="table mx-sm-3 mb-2 m-4">
+        <thead class="table-dark">
+            <tr>
+                <th>Código</th>
+                <th>URL Original</th>
+                <th>URL Acortada</th>
+            </tr>
+        </thead>
+        <tbody class="table-active border border-dark">
+            @if(!empty($urls))
+            @foreach($urls as $url)
+            <tr>
+                <td>{{ $url['codigo'] }}</td>
+                <td> <a href="{{ $url['urlOriginal'] }}">{{ $url['urlAcortada']  }}</a>
+                </td>
+                <td>{{ $url['urlOriginal'] }}</td>
+            </tr>
+            @endforeach
+            @endif
+        </tbody>
+    </table>
+</body>
+
+</html>
